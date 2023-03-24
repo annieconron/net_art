165c003d8c07e1b1aa2be809b081146ddf453a25
annie conron <s3900816@student.rmit.edu.au>
2023-03-24 16:53:20 +1100

    added a pink square

--- public/sketch.js
+++ public/sketch.js
@@ -3,8 +3,13 @@ document.body.style.overflow = `hidden`
 
 function setup () {
    createCanvas (innerWidth, innerHeight)
+   fill ('pink')
+   rectMode (CENTER)
+   noStroke()
+   noLoop()
 }
 
 function  draw () {
    background (`turquoise`)
+   square(width/2, height/2, 100)
 }
