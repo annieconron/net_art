94441db4138922d0a53368d7871d47121d345c91
annie conron <s3900816@student.rmit.edu.au>
2023-05-12 16:32:28 +1000

    update

--- public/branch.js
+++ public/branch.js
@@ -1,24 +1,28 @@
-//Branch Class
 function Branch(begin, end) {
+  // Constructor function used to create branch objects in the fractal tree.
   this.begin = begin;
   this.end = end;
+  // The 'begin' and 'end' parameters represent the starting and ending points of the branch
   this.finished = false;
-  this.falling = false; // new property
+  // This property indicates whether the branch has completed its growth
 
   this.jitter = function() {
+  // Displacement to the end point of the branch, creating a jittering effect
     this.end.x += random(-0.5, 0.5);
     this.end.y += random(-0.5, 0.5);
   }
   
   this.show = function() {
+  // Responsible for displaying the branch as a line with a specified color and stroke weight
     stroke(102, 51, 0);
     strokeWeight(4);
     line(this.begin.x, this.begin.y, this.end.x, this.end.y); 
   }
    
   this.branchA = function () {
+  // calculates a new branch (a) by determining a direction vector from the current branch and rotating it by a predefined angle (PI / 8). The length of the new branch is reduced by a factor of 0.67. The new branch is then created using the updated end point
     var dir = p5.Vector.sub(this.end, this.begin);
-    dir.rotate(PI / 6);
+    dir.rotate(PI / 8);
     dir.mult(0.67);
     var newEnd = p5.Vector.add(this.end, dir);
     var a = new Branch(this.end, newEnd);
@@ -26,11 +30,13 @@ function Branch(begin, end) {
   }
   
   this.branchB = function () {
+    // function is similar to branchA but rotates the direction vector in the opposite direction (-PI / 9) to create a different branch (b)
     var dir = p5.Vector.sub(this.end, this.begin);
-    dir.rotate(-PI / 4);
+    dir.rotate(-PI / 9);
     dir.mult(0.67);
     var newEnd = p5.Vector.add(this.end, dir);
     var b = new Branch(this.end, newEnd);
     return b;
   }
 }
+
--- public/sketch.js
+++ public/sketch.js
@@ -1,30 +1,34 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
-var tree = [];
-var leaves = [];
-var fallingBranches = []; // new array
-
-var count = 0;
+var tree = []; // Array to store branches of the tree
+var leaves = []; // Array to store leaves of the tree
+var fallingBranches = []; // Array to store falling branches
+var count = 0; // Counter for tracking number of times mouse is pressed
 
 function setup() {
-  let w = window.innerWidth;
-  let h = window.innerHeight;
-  createCanvas(w, h);;
-    
+  let w = windowWidth;
+  let h = windowHeight;
+  createCanvas(w, h);
+  
+  // Create root branch at the center bottom of the canvas
   var a = createVector(width/2, height);
   var b = createVector(width/2, height-200);
-  
   root = new Branch(a, b);
-  
+  // Add the root to the tree array
   tree[0] = root;
 }
 
-function windowResized () {
-  resizeCanvas (window.innerWidth, window.innerHeight)
-}
-
 function mousePressed() {
+  // Generate new branches from existing branches and mark the previous ones as finished
+  for (var i = tree.length-1; i >= 0; i--) {
+    if (!tree[i].finished) {
+      tree.push(tree[i].branchA());
+      tree.push(tree[i].branchB());
+      tree[i].finished = true;
+    }
+  }
+  // The same code block is repeated so there is two generations per mouse press
   for (var i = tree.length-1; i >= 0; i--) {
     if (!tree[i].finished) {
       tree.push(tree[i].branchA());
@@ -33,57 +37,62 @@ function mousePressed() {
     }
   }
   
-  count++;
+  count++; // Increment counter
   
-  if(count === 8){
+  // Attach leaves to the last branches in the array after 4 mouse presses
+  if(count === 4){
     for (var i = 0; i < tree.length; i++){
       if (!tree[i].finished) {
          var leaf = tree[i].end.copy();
          leaves.push(leaf);
       }
     }
-    dropBranches(); // call the new function
+    dropBranches(); // Call the function to drop branches
   }
 }
 
 function mouseDragged() {
+  // Give jitter effect to branches on mouse drag
   for (var i = 3; i < tree.length; i++) {
     tree[i].jitter();
   }
 }
 
 function dropBranches() {
+  // Move branches to the fallingBranches array for animation
   for (var i = 1; i < tree.length; i++) {
     fallingBranches.push(tree[i]);
   }
-  tree.splice(1); // remove branches from index 1 onwards
+  tree.splice(1); // Remove branches from index 1 onwards
 }
 
 function draw() {
-  background(204, 226, 255);
+  background(204, 226, 255); 
   
+  // Display all branches of the tree
   for (var i = 0; i < tree.length; i++) {
     tree[i].show();
   }
   
   for (var i = 0; i < leaves.length; i++) {
+    // Display all leaves with animation
     fill(0, 102, 51, 80);
     noStroke();
     ellipse(leaves[i].x, leaves[i].y, 12);
-    leaves[i].y += random(0, 2);
+    leaves[i].y += random(-2, 6);
+    leaves[i].x += random(-1, 1);
   }
 
-  // update position of falling branches
+  // Update position of falling branches and animate them
   for (var i = 0; i < fallingBranches.length; i++) {
     var branch = fallingBranches[i];
-    //branch.show();
-    line(branch.end.x, branch.end.y, branch.end.x-4, branch.end.y-4)
-    stroke(102, 51, 0);
-    strokeWeight(4);
-    branch.end.y += random(0, 2);
+    ellipse(branch.end.x, branch.end.y, 4);
+    fill(102, 51, 0, 95);
+    noStroke();
+    branch.end.y += random(-2, 6);
   }
 
-  // remove branches that have fallen to the bottom
+  // Remove branches that have fallen to the bottom
   for (var i = fallingBranches.length - 1; i >= 0; i--) {
     var branch = fallingBranches[i];
     if (branch.end.y >= height) {
@@ -91,8 +100,8 @@ function draw() {
     }
   }
 
-//reset count and generate branches again when all branches have fallen
-  if (fallingBranches.length === 0 && count === 8) {
+  //Reset count and generate branches again when all branches have fallen
+  if (fallingBranches.length === 0 && count === 4) {
     count = 0;
     var a = createVector(width/2, height);
     var b = createVector(width/2, height-200);
